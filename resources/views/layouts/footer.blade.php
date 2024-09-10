b1ee9b500c2759df7ccb4f8fa1bcafe0a4a605c3
Muhammad Alif Firdaus <alifmuhammadfirdaus1@gmail.com>
2024-09-10 11:42:21 +0700

    fc

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,17 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-6">
+                <script>
+                    document.write(new Date().getFullYear())
+                </script> © Alif.
+            </div>
+            <div class="col-sm-6">
+                <div class="text-sm-end d-none d-sm-block">
+                    Develop by <a href="https://www.linkedin.com/in/alif-firdauzz/" target="_blank">Alif</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+</div>
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,39 @@
+<!-- ========== Left Sidebar Start ========== -->
+<div class="vertical-menu">
+
+    <div data-simplebar class="h-100">
+
+        <!--- Sidemenu -->
+        <div id="sidebar-menu">
+            <!-- Left Menu Start -->
+            <ul class="metismenu list-unstyled" id="side-menu">
+                <li class="menu-title" key="t-menu">Menu</li>
+
+                <li>
+                    <a href="{{'/'}}}" class="waves-effect">
+                        <i class="bx bx-home-circle"></i>
+                        <span key="t-dashboards">Dashboard</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <i class="bx bx-layout"></i>
+                        <span key="t-layouts">Layouts</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="true">
+                        <li>
+                            <a href="javascript: void(0);" class="has-arrow" key="t-vertical">Vertical</a>
+                        </li>
+
+                        <li>
+                            <a href="javascript: void(0);" class="has-arrow" key="t-horizontal">Horizontal</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        <!-- Sidebar -->
+    </div>
+</div>
+<!-- Left Sidebar End -->
